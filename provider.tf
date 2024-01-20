b74b02e5eb2b5443c275bb5f1e332cbfe5d60dbe
annanange <156485925+annanange@users.noreply.github.com>
2024-01-20 10:50:25 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = "us-east-1"
+vpc_id = "vpc-0495e1747697a7a0d"
  // access_key              = "${env.AWS_ACCESS_KEY_ID}" 
  // secret_key              = "${env.AWS_SECRET_ACCESS_KEY}"
   # profile = "effulgencetech"
